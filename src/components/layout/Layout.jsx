a369dd49a47cc91faf2000389e3a04fec5a777da
Julian.Oliva <102344556+JNO-22@users.noreply.github.com>
2025-02-06 19:42:55 -0300

    ..

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Alerta from "@components/ui/alert/Alert";
 import Loading from "@components/ui/loading/Loading";
 import useClearAlertOnPathChange from "@hooks/useClearUiPathChange";
-import NavBar from "@components/layout/NavBar";
+import NavBar from "../layout/Navbar";
 
 const Layout = () => {
   useClearAlertOnPathChange();
