43d5760f7e2bf739e1db74184b72b209e7e05c21
Julian.Oliva <102344556+JNO-22@users.noreply.github.com>
2025-02-06 19:30:00 -0300

    Fix navbar import

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import Alerta from "@components/ui/alert/Alert";
 import Loading from "@components/ui/loading/Loading";
 import useClearAlertOnPathChange from "@hooks/useClearUiPathChange";
-import NavBar from "./NavBar";
+import NavBar from "@components/layout/NavBar";
 
 const Layout = () => {
   useClearAlertOnPathChange();
